4ca0f8ab9f550f5fcdf120fe4e6844a1d99d5545
Mohit Maheshwari <bhutramohit@gmail.com>
2025-06-10 18:28:26 +0530

    Test changes

--- src/pages/apps/personalDetails/personalDetails.tsx
+++ src/pages/apps/personalDetails/personalDetails.tsx
@@ -286,10 +286,12 @@ const PersonalDetails: React.FC = () => {
       const response = await extractPDFData(formdata);
       const responseData = response.data as ResponsePDFData;
       const pdfData = responseData.data;
+      //Tab 1
       setFullName(pdfData.name || '');
       setTimeOfBirth(pdfData.timeOfBirth || '');
       setDateOfBirth(pdfData.dateOfBirth ? dayjs(pdfData.dateOfBirth, 'DD/MM/YYYY').format('YYYY-MM-DD') : null);
       setPlaceOfBirth(pdfData.placeOfBirth || '');
+      //Tab 2
       setGender(pdfData.gender || '');
       setDisability(pdfData.disability || '');
       setHeight(pdfData.heightCM || '');
@@ -298,9 +300,11 @@ const PersonalDetails: React.FC = () => {
       setHobbies(pdfData.hobbies || []);
       setComplexion(pdfData.complexion || '');
       setMaritalStatus(pdfData.maritalStatus || '');
+      //Tab 3      
       setDietaryHabits(pdfData.dietary || '');
       setDrinking(pdfData.drinking || '');
       setSmoking(pdfData.smoking || '');
+      //Tab 4
       setFatherName(pdfData.fatherName || '');
       setMotherName(pdfData.motherName || '');
       setHometown(pdfData.nativePlace || '');
